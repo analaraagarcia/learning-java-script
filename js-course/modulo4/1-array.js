e0b7f161bcd7c381adcbc89708735ba0f09e29e6
Ana Lara Garcia <analaraagarcia@hotmail.com>
2024-10-24 10:21:11 -0300

    new classes

---
+++ js-course/modulo4/1-array.js
@@ -0,0 +1,14 @@
+// arrays
+
+const alunos = ["Ana", "Luis", "Manu"];
+console.log(alunos[0]);
+
+alunos.push("Lara");
+alunos[3] = "Joao";
+console.log(alunos);
+
+alunos.pop(); // tira o ultimo
+console.log(alunos);
+
+alunos.shift(); // tira o primeiro
+console.log(alunos);
---
+++ js-course/modulo4/2-estrutura-repeticao.js
@@ -0,0 +1,18 @@
+const notas = [];
+notas.push(5);
+notas.push(8);
+notas.push(10);
+notas.push(9);
+notas.push(7);
+
+let soma = 0;
+
+// estrutura de repetição
+for (let i = 0; i < notas.length; i++) {
+  const nota = notas[i];
+  soma += nota;
+}
+
+let media = soma / notas.length;
+
+console.log(media);
